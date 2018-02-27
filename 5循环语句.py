cb9461645fee1a8e165528d9b8fd2a1547d80d3e
Ken <wangken33@163.com>
2018-02-28 00:02:22 +0800

    20180228.01
    20180228.01

--- 5循环语句.py
+++ 5循环语句.py
@@ -42,8 +42,8 @@
 # else:
 #     print ('恭喜猜中')
 
-# 练习二：猜拳
 
+# 练习二：猜拳
 # import random
 # num = 1
 # while num <= 20:
@@ -52,35 +52,54 @@
 #     print (s)
 #     num += 1
 
-import random
-computerlist = ['jiandao','shitou','bu']
-while 1:
-    s = int(random.randint(1, 3))
-    if s == 1:
-        ind = 'jiandao'
-    if s == 2:
-        ind = 'shitou'
-    if s == 3:
-        ind = 'bu'
+# import random
+# computerlist = ['jiandao','shitou','bu']
+# while 1:
+#     s = int(random.randint(1, 3))
+#     if s == 1:
+#         ind = 'jiandao'
+#     if s == 2:
+#         ind = 'shitou'
+#     if s == 3:
+#         ind = 'bu'
+#
+#     m = input('please input shitou or jiandao or bu，input \'end\' sign out：')
+#
+#     if ((m != 'end') and (m not in computerlist)):
+#         print ('input error,please again input!')
+#         continue
+#     elif m == 'end':
+#         print ('sign out game!')
+#         break
+#     elif ((m == 'jiandao' and ind == 'jiandao') or (m == 'shitou' and ind == 'shitou') or (m == 'bu' and ind == 'bu')):
+#         print ('computer is ' + ind + '，ping ju!')
+#         continue
+#     elif ((m == 'jiandao' and ind == 'bu') or (m == 'shitou' and ind == 'jiandao') or (m == 'bu' and ind == 'shitou')):
+#         print ('computer is ' + ind + '，you are win!')
+#         continue
+#     elif ((m == 'jiandao' and ind == 'shitou') or (m == 'shitou' and ind == 'bu') or (m == 'bu' and ind == 'jiandao')):
+#         print ('computer is ' + ind + '，you are fail!')
+#         continue
 
-    m = input('please input shitou or jiandao or bu，input \'end\' sign out：')
 
-    if ((m != 'end') and (m not in computerlist)):
-        print ('input error,please again input!')
-        continue
-    elif m == 'end':
-        print ('sign out game!')
-        break
-    elif ((m == 'jiandao' and ind == 'jiandao') or (m == 'shitou' and ind == 'shitou') or (m == 'bu' and ind == 'bu')):
-        print ('computer is ' + ind + '，ping ju!')
-        continue
-    elif ((m == 'jiandao' and ind == 'bu') or (m == 'shitou' and ind == 'jiandao') or (m == 'bu' and ind == 'shitou')):
-        print ('computer is ' + ind + '，you are win!')
-        continue
-    elif ((m == 'jiandao' and ind == 'shitou') or (m == 'shitou' and ind == 'bu') or (m == 'bu' and ind == 'jiandao')):
-        print ('computer is ' + ind + '，you are fail!')
-        continue
 
+# 练习三：九九乘法表
+rownum = 1
+
+while rownum < 10:
+
+    firstnum = 1
+    str = ''
+
+    while firstnum <= rownum:
+        if firstnum != rownum:
+            str = "%s%s*%s=%s,"%(str,firstnum,rownum,firstnum * rownum)
+        else:
+            str = "%s%s*%s=%s" % (str, firstnum, rownum, firstnum * rownum)
+        firstnum += 1
+
+    print(str)
+    rownum += 1
 
 
 
